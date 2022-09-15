684975e6cd18cf592785ce5c87cf13952bfb1b87
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-09-16 01:22:51 +0800

    add new line for examples

--- lpthw/ex1.py
+++ lpthw/ex1.py
@@ -2,14 +2,21 @@ print("Hello, world")
 print("Hello, again")
 print("I like typing this")
 print("this is me having fun")
-print(" Yay1 printing")
+print("Yay printing")
 print("i'd much rather you not")
-print(" i say do not tiuch this")
+print("i say do not touch this")
 
 
 data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
 pos = data.find('.')
-print(data[pos:pos+3]) #exclusive the end points
+print(pos)
+print(data[pos:pos+3]) # exclusive the end point
+
+name = "Siti Nur Athiffah binti Ayub"
+nickname = name.find("A")
+print(name[nickname:nickname+8])
+
+
 
 text = "X-DSPAM-Confidence:    0.8475"
 
